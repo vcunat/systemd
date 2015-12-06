bdf7c152cddda0a7da0835c1b931b7ecc5c41de1
Gabriel Ebner <gebner@gebner.org>
2015-12-06 14:26:36 +0100

    hostnamed, localed, timedated: disable methods that change system settings.

--- src/hostname/hostnamed.c
+++ src/hostname/hostnamed.c
@@ -415,6 +415,9 @@ static int method_set_hostname(sd_bus_message *m, void *userdata, sd_bus_error *
         if (r < 0)
                 return r;
 
+        return sd_bus_error_setf(error, SD_BUS_ERROR_NOT_SUPPORTED,
+            "Changing system settings via systemd is not supported on NixOS.");
+
         if (isempty(name))
                 name = c->data[PROP_STATIC_HOSTNAME];
 
@@ -473,6 +476,9 @@ static int method_set_static_hostname(sd_bus_message *m, void *userdata, sd_bus_
         r = sd_bus_message_read(m, "sb", &name, &interactive);
         if (r < 0)
                 return r;
+
+        return sd_bus_error_setf(error, SD_BUS_ERROR_NOT_SUPPORTED,
+            "Changing system settings via systemd is not supported on NixOS.");
 
         if (isempty(name))
                 name = NULL;
@@ -540,6 +546,9 @@ static int set_machine_info(Context *c, sd_bus_message *m, int prop, sd_bus_mess
         r = sd_bus_message_read(m, "sb", &name, &interactive);
         if (r < 0)
                 return r;
+
+        return sd_bus_error_setf(error, SD_BUS_ERROR_NOT_SUPPORTED,
+            "Changing system settings via systemd is not supported on NixOS.");
 
         if (isempty(name))
                 name = NULL;
